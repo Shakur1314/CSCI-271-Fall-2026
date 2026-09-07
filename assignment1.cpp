bdf4eb31f10f2b9f9c87739297b73432d2d6ecbd
Shakur Ouro <shakur.ourogouni@stu.bcc.cuny.edu>
2026-09-07 00:37:51 -0400

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Assignment 1 - Shakur OuroGouni
+int main() {
+    cout << "Hello, world!\n";
+    
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+    
+    cout << "Nice to meet you, " << name << "!\n";
+    
+    return 0;
+}
